5e3c373b72c3df6e53509ec04352c55459c4c45e
said1573 <therapy23@naver.com>
2019-01-22 10:39:48 +0900

    eletronics

---
+++ app/urls.py
@@ -0,0 +1,67 @@
+"""eletronics URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from app import views
+
+urlpatterns = [
+    path('calendar/', views.calendar, name='calendar'),
+    path('chartjs/', views.chartjs, name='chartjs'),
+    path('chartjs2/', views.chartjs2, name='chartjs2'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('e_commerce/', views.e_commerce, name='e_commerce'),
+    path('echarts/', views.echarts, name='echarts'),
+    path('fixed_footer/', views.fixed_footer, name='fixed_footer'),
+    path('fixed_sidebar/', views.fixed_sidebar, name='fixed_sidebar'),
+    path('form/', views.form, name='form'),
+    path('form_advanced/', views.form_advanced, name='form_advanced'),
+
+    path('form_buttons/', views.form_buttons, name='form_buttons'),
+    path('form_upload/', views.form_upload, name='form_upload'),
+    path('form_validation/', views.form_validation, name='form_validation'),
+    path('form_wizards/', views.form_wizards, name='form_wizards'),
+    path('general_elements/', views.general_elements, name='general_elements'),
+    path('glyphicons/', views.glyphicons, name='glyphicons'),
+    path('icons/', views.icons, name='icons'),
+    path('inbox/', views.inbox, name='inbox'),
+    path('index/', views.index, name='index'),
+    path('index2/', views.index2, name='index2'),
+
+    path('index3/', views.index3, name='index3'),
+    path('invoice/', views.invoice, name='invoice'),
+    path('level2/', views.level2, name='level2'),
+    path('login/', views.login, name='login'),
+    path('map/', views.map, name='map'),
+    path('media_gallery/', views.media_gallery, name='media_gallery'),
+    path('morisjs/', views.morisjs, name='morisjs'),
+    path('other_charts/', views.other_charts, name='other_charts'),
+    path('page_403/', views.page_403, name='page_403'),
+    path('page_404/', views.page_404, name='page_404'),
+
+    path('page_500/', views.page_500, name='page_500'),
+    path('plain_page/', views.plain_page, name='plain_page'),
+    path('pricing_tables/', views.pricing_tables, name='pricing_tables'),
+    path('profile/', views.profile, name='profile'),
+    path('project_detail/', views.project_detail, name='project_detail'),
+    path('projects/', views.projects, name='projects'),
+    path('tables/', views.tables, name='tables'),
+    path('tables_dynamic/', views.tables_dynamic, name='tables_dynamic'),
+    path('typography/', views.typography, name='typography'),
+    path('widgets/', views.widgets, name='widgets'),
+
+    path('xx/', views.xx, name='xx'),
+]
---
+++ app/views.py
@@ -0,0 +1,145 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def calendar(request):
+    return render(request, 'app/calendar.html', {})
+
+def chartjs(request):
+    return render(request, 'app/chartjs.html', {})
+
+def chartjs2(request):
+    return render(request, 'app/chartjs2.html', {})
+
+def contacts(request):
+    return render(request, 'app/contacts.html', {})
+
+def e_commerce(request):
+    return render(request, 'app/e_commerce.html', {})
+
+def echarts(request):
+    return render(request, 'app/echarts.html', {})
+
+def fixed_footer(request):
+    return render(request, 'app/fixed_footer.html', {})
+
+def fixed_sidebar(request):
+    return render(request, 'app/fixed_sidebar.html', {})
+
+def form(request):
+    return render(request, 'app/form.html', {})
+
+def form_advanced(request):
+    return render(request, 'app/form_advanced.html', {})
+
+
+
+
+
+def form_buttons(request):
+    return render(request, 'app/form_buttons.html', {})
+
+def form_upload(request):
+    return render(request, 'app/form_upload.html', {})
+
+def form_validation(request):
+    return render(request, 'app/form_validation.html', {})
+
+def form_wizards(request):
+    return render(request, 'app/form_wizards.html', {})
+
+def general_elements(request):
+    return render(request, 'app/general_elements.html', {})
+
+def glyphicons(request):
+    return render(request, 'app/glyphicons.html', {})
+
+def icons(request):
+    return render(request, 'app/icons.html', {})
+
+def inbox(request):
+    return render(request, 'app/inbox.html', {})
+
+def index(request):
+    return render(request, 'app/index.html', {})
+
+def index2(request):
+    return render(request, 'app/index2.html', {})
+
+
+
+
+
+def index3(request):
+    return render(request, 'app/index3.html', {})
+
+def invoice(request):
+    return render(request, 'app/invoice.html', {})
+
+def level2(request):
+    return render(request, 'app/level2.html', {})
+
+def login(request):
+    return render(request, 'app/login.html', {})
+
+def map(request):
+    return render(request, 'app/map.html', {})
+
+def media_gallery(request):
+    return render(request, 'app/media_gallery.html', {})
+
+def morisjs(request):
+    return render(request, 'app/morisjs.html', {})
+
+def other_charts(request):
+    return render(request, 'app/other_charts.html', {})
+
+def page_403(request):
+    return render(request, 'app/page_403.html', {})
+
+def page_404(request):
+    return render(request, 'app/page_404.html', {})
+
+
+
+
+
+def page_500(request):
+    return render(request, 'app/page_500.html', {})
+
+def plain_page(request):
+    return render(request, 'app/plain_page.html', {})
+
+def pricing_tables(request):
+    return render(request, 'app/pricing_tables.html', {})
+
+def profile(request):
+    return render(request, 'app/profile.html', {})
+
+def project_detail(request):
+    return render(request, 'app/project_detail.html', {})
+
+def projects(request):
+    return render(request, 'app/projects.html', {})
+
+def tables(request):
+    return render(request, 'app/tables.html', {})
+
+def tables_dynamic(request):
+    return render(request, 'app/tables_dynamic.html', {})
+
+def typography(request):
+    return render(request, 'app/typography.html', {})
+
+def widgets(request):
+    return render(request, 'app/widgets.html', {})
+
+
+
+
+
+def xx(request):
+    return render(request, 'app/xx.html', {})
+
+
